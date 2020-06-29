68983b3c06e2f90f26f0663bfe9806e31ef21e2a
Satish Prasad <spd61185@gmail.com>
2020-06-29 20:40:58 +0530

    Matrix Program

---
+++ Interview_2020/src/com/comapny/hcl/PrintMatrix.java
@@ -0,0 +1,23 @@
+package com.comapny.hcl;
+/*
+            313
+			323
+			333
+
+  */
+public class PrintMatrix {
+	public static void main(String[] args) {
+		int i,j,k=1,n=3;
+		for(i=0;i<n;i++) {
+			for(j=0;j<n;j++) {
+				if(j == 1 && i > 0) {
+					System.out.print(k);
+					k++;
+				}else {
+					System.out.print(n);
+				}
+			}
+			System.out.println();
+		}
+	}
+}
