559a771bc0caa870cc0d12f84fa1e3d5661e5eb7
Satish Prasad <spd61185@gmail.com>
2020-07-19 20:58:37 +0530

    Aris Global

---
+++ Interview_2020/src/com/company/arisglobal/Main.java
@@ -0,0 +1,15 @@
+package com.company.arisglobal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Animal{
+
+}
+class Dog extends Animal{}
+public class Main {
+	public static void main(String[] args) {
+		List<Animal>list = new ArrayList<>();
+		Add(list);
+	}
+}
