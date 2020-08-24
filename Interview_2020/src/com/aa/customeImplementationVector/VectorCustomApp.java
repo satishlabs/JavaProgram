8300fa6b6c39a4eec230cc8e625e4567bd9e9022
Satish Prasad <spd61185@gmail.com>
2020-08-24 17:55:10 +0530

    Test

--- Interview_2020/src/com/aa/customeImplementationVector/VectorCustomApp.java
+++ Interview_2020/src/com/aa/customeImplementationVector/VectorCustomApp.java
@@ -4,12 +4,12 @@ public class VectorCustomApp {
 	public static void main(String[] args) {
 		VectorCustom<Integer>  list = new VectorCustom<Integer>();
 		list.add(1);
-	     list.add(2);
-	     list.add(3);
-	     list.add(4);
-	     list.add(1);
-	     list.add(2);
-	     list.display();
+	    list.add(2);
+	    list.add(3);
+	    list.add(4);
+	    list.add(1);
+	    list.add(2);
+	    list.display();
 	     
 	     System.out.println("\nelement at index "+1+" = "+list.get(1));
 	     System.out.println("element removed from index "+1+" = "+list.remove(1));
